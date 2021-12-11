a967ebfa1391789855c929100d71ca9dbefb0ac3
RostyslavDubinin <75220575+RostyslavDubinin@users.noreply.github.com>
2021-12-11 17:56:34 +0200

    don

---
+++ controllers/auth/getCurrentUser.js
@@ -0,0 +1,11 @@
+const getCurrentUser = async (req, res, next) => {
+    const { email, balance } = req.user;
+    res.json({
+      status: "success",
+      code: 200,
+      email,
+      balance,
+    });
+  };
+  
+  module.exports = getCurrentUser;
