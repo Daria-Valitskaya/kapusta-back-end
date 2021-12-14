1f496872861c8ce867d11949d09ec30d8b06ace4
YuliaGordienko <75683718+YuliaGordienko@users.noreply.github.com>
2021-12-14 20:13:28 +0200

    add swagger

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require("express");
+
 const logger = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 
-const authRouter = require('./routes/api/auth')
-const balanceRouter = require('./routes/api/user')
+const authRouter = require("./routes/api/auth");
+const balanceRouter = require("./routes/api/user");
 const app = express();
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
-app.use('/api/auth', authRouter);
-app.use('/api/user', balanceRouter);
+app.use(express.static("public"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", balanceRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
