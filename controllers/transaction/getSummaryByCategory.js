9fc2693db7762e88495b1e191d718d7f3041c4f8
AleksandrTsyhankov <74210910+AleksandrTsyhankov@users.noreply.github.com>
2021-12-19 13:41:47 +0200

    update

--- controllers/transaction/getSummaryByCategory.js
+++ controllers/transaction/getSummaryByCategory.js
@@ -5,13 +5,13 @@ const { getTransSummaryByCategory } = require('../../helpers/index');
 
 const getSummaryByCategory = async (req, res, next) => {
   
-  const { type } = req.params;
+  const { type, date } = req.params;
   const { _id } = req.user;
   const data = await Transaction.find({ owner: _id, transactionType: type });
 
   const userData = data.filter(elem => String(elem.owner) === String(_id));
 
-  const result = getTransSummaryByCategory(userData, type);
+  const result = getTransSummaryByCategory( date, userData, type);
 
   if (result.length === 0) {
     throw new NotFound(`Not found`);
