43cd0d920e4dd36ad40f6d88a3b53e176641ab13
Chernykh Oksana <spec20@ukr.net>
2021-12-22 12:58:16 +0200

    код по логину(нужно фиксить)

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -1,21 +1,22 @@
-// const queryString = require("query-string");
+const queryString = require("query-string");
 // const URL = require("url");
+// const { GOOGLE_CLIENT_ID } = process.env;
 
-// const googleAuth = async (req, res) => {
-//   const stringifiedParams = queryString.stringify({
-//     client_id: process.env.GOOGLE_CLIENT_ID,
-//     redirect_uri: `${process.env.BASE_URL}/api/user/google-redirect`,
-//     scope: [
-//       "https://www.googleapis.com/auth/userinfo.email",
-//       "https://www.googleapis.com/auth/userinfo.profile",
-//     ].join(" "),
-//     response_type: "code",
-//     access_type: "offline",
-//     prompt: "consent",
-//   });
-//   return res.redirect(
-//     `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
-//   );
-// };
+const googleAuth = async (req, res) => {
+  const stringifiedParams = queryString.stringify({
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    redirect_uri: `https://kapusta-team-project-back.herokuapp.com/api/auth/google-redirect`,
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.email",
+      "https://www.googleapis.com/auth/userinfo.profile",
+    ].join(" "),
+    response_type: "code",
+    access_type: "offline",
+    prompt: "consent",
+  });
+  return res.redirect(
+    `https://accounts.google.com/o/oauth2/v2/auth?${stringifiedParams}`
+  );
+};
 
-// module.exports = googleAuth;
+module.exports = googleAuth;
--- controllers/auth/googleRedirect.js
+++ controllers/auth/googleRedirect.js
@@ -1,37 +1,70 @@
-// const queryString = require("query-string");
-// const axios = require("axios");
-// // const URL = require("url");
+const queryString = require("query-string");
+const axios = require("axios");
+require("dotenv").config();
+const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = process.env;
+const { nanoid } = require("nanoid");
 
-// exports.googleRedirect = async (req, res) => {
-//   const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
-//   const urlObj = new URL(fullUrl);
-//   const urlParams = queryString.parse(urlObj.search);
-//   const code = urlParams.code;
-//   console.log(code);
+const googleRedirect = async (req, res) => {
+  const fullUrl = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
+  const urlObj = new URL(fullUrl);
+  const urlParams = queryString.parse(urlObj.search);
+  const code = urlParams.code;
+  console.log(code);
 
-//   const tokenData = await axios({
-//     url: `https://oauth2.googleapis.com/token`,
-//     method: "post",
-//     data: {
-//       client_id: process.env.GOOGLE_CLIENT_ID,
-//       client_secret: process.env.GOOGLE_CLIENT_SECRET,
-//       redirect_uri: `${process.env.BASE_URL}/auth/google-redirect`,
-//       grant_type: "authorization_code",
-//       code,
-//     },
-//   });
-//   const userData = await axios({
-//     url: "https://www.googleapis.com/oauth2/v2/userinfo",
-//     method: "get",
-//     headers: {
-//       Authorization: `Bearer ${tokenData.data.access_token}`,
-//     },
-//   });
-//   // userData.data.email
+  const tokenData = await axios({
+    url: `https://oauth2.googleapis.com/token`,
+    method: "post",
+    data: {
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      redirect_uri: `https://kapusta-team-project-back.herokuapp.com/api/auth/google-redirect`,
+      grant_type: "authorization_code",
+      code,
+    },
+  });
+  const userData = await axios({
+    url: "https://www.googleapis.com/oauth2/v2/userinfo",
+    method: "get",
+    headers: {
+      Authorization: `Bearer ${tokenData.data.access_token}`,
+    },
+  });
 
-//   return res.redirect(
-//     `${process.env.FRONTEND_URL}?email=${userData.data.email}`
-//   );
-// };
+  const { name, email } = userData.data;
+  const user = await User.findOne({ email });
+  console.log(user);
+  if (!user) {
+    const newUser = await User.create({
+      name,
+      email,
+      verify: true,
+    });
+    newUser.setPassword(nanoid());
+    await newUser.save();
 
-// module.exports = googleRedirect;
+    const payload = {
+      id: user._id,
+    };
+    const token = jwt.sign(payload, SECRET_KEY);
+    const { _id } = newUser;
+    await User.findByIdAndUpdate(user._id, { token });
+
+    return res.redirect(
+      `https://kapusta-team-project-front.netlify.app/?access_token=${token}&email=${userData.data.email}`
+    );
+  }
+
+  const payload = {
+    id: user._id,
+  };
+  const token = jwt.sign(payload, SECRET_KEY);
+  await User.findByIdAndUpdate(user._id, { token });
+
+  return res.redirect(
+    `https://kapusta-team-project-front.netlify.app/api/users/google-redirect/?access_token=${token}`
+  );
+};
+
+module.exports = googleRedirect;
